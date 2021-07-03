70799be4ccbc070bae0c52001860e4ad43ba428a
thugfuto53 <86662632+thugfuto53@users.noreply.github.com>
2021-07-03 17:34:09 +0900

    Add files via upload

---
+++ Main1375.java
@@ -0,0 +1,23 @@
+public class Main1375{
+    public static void main(String[] args){
+        System.out.print("aaaaaaaaaaaaaaaaaaaaa");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch(selected){
+            case 1:
+            System.out.print("aaaaaaaaaaaaaaaaaaaaa");
+            break;
+
+            case 2:
+            System.out.print("aaaaaaaaaaaaaaaaaaaaa");
+            break;
+
+            case 3:
+            System.out.print("aaaaaaaaaaaaaaaaaaaaa"); 
+            break;
+
+            case 4:
+            System.out.print("aaaaaaaaaaaaaaaaaaaaa");
+            break;
+        }
+    }
+}
