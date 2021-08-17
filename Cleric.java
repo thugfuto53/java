675c83dff6ce6d9a72e24a15a136202c84330bcd
thugfuto53 <86662632+thugfuto53@users.noreply.github.com>
2021-08-17 22:50:47 +0900

    Update Cleric.java

--- Cleric.java
+++ Cleric.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class Cleric {
-    String name = "kurelic";
+    String name = "クレリック";
     int hp = 50;
     final int maxhp = 50;
     int mp = 10;
     final int maxmp = 10;
     public void selfAid(){
-        System.out.println(this.name + "ha seruhueidowo tonaeta!");
+        System.out.println(this.name + "はセルフエイドをとなえた！");
         this.hp = this.maxhp;
         this.mp -= 5;
-        System.out.println("HPgasaidaimadekaihukusita");
+        System.out.println("HPが最大まで回復した");
     }
     public int pray(int sec){
-        System.out.println(this.name + "ha" + sec + "byoukann ten niinotta!");
+        System.out.println(this.name + "は" + sec + "秒間天に祈った！");
         int recover = new java.util.Random().nextInt(3) + sec;
         int recoverActual = Math.min(this.maxhp - this.mp, recover);
         this.mp += recoverActual;
-        System.out.println("MPga" + recoverActual + "kaihukusita");
+        System.out.println("MPが" + recoverActual + "回復した");
         return recoverActual;
     }
 }
